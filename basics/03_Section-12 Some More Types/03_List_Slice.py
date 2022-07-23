d208bfe823631862d67263324aeb1966e3493f19
ajmalrashadmarkz <ajmalrashadmarkz@gmail.com>
2022-07-23 12:02:40 +0530

    Diffrent List Slicing Methods

---
+++ basics/03_Section-12 Some More Types/03_List_Slice.py
@@ -0,0 +1,20 @@
+# Create the List
+x=[20, 30, 40, 10, 50,8,9,0,5]
+
+# Print All List
+print(x)
+
+#Print the  1st element to 4th element
+print(x[1:5])
+
+#Print the 1st element to 6th element
+print (x[2:7])
+
+#Print the 0th element to 7th element
+print(x[:8])
+
+#Print the 2nd element to till end
+print(x[2:])
+
+#Print the 1st element to 7th element [step is 2 - one after another]
+print(x[1:8:2])
