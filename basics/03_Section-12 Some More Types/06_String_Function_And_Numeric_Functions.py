7e827982eb87cae7109e5b6747718ee1f2750d24
ajmalrashadmarkz <ajmalrashadmarkz@gmail.com>
2022-07-23 15:00:09 +0530

    String & Numeric Functions like upper(),lower(),startswith(),endswith(),min(),max(),abs()

---
+++ basics/03_Section-12 Some More Types/06_String_Function_And_Numeric_Functions.py
@@ -0,0 +1,23 @@
+newstring = "hello am Mark"
+print(newstring.upper())
+
+newstring = "Hello Am mark"
+print(newstring.lower())
+
+newstring = "hello am mark"
+print(newstring.startswith("h"))
+
+newstring = "hello am mark"
+print(newstring.startswith("p"))
+
+newstring = "hello am mark"
+print(newstring.endswith("k"))
+
+newstring = "hello am mark"
+print(newstring.endswith("j"))
+
+print(min(2,4,1,6,7))
+
+print(max(2,4,1,6,7))
+
+print(abs(-22))
